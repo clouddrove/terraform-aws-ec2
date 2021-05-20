e0f6a0bca453d85a4c6cd651a527b935f927c7ff
yadavprakash <yadavprakash.it@gmail.com>
2021-05-20 21:25:08 +0530

    enable-encrypted

--- _example/basic_example/example.tf
+++ _example/basic_example/example.tf
@@ -107,7 +107,6 @@ module "ec2" {
   instance_count              = 2
   ami                         = "ami-08d658f84a6d84a80"
   instance_type               = "t2.nano"
-  monitoring                  = false
   tenancy                     = "default"
   vpc_security_group_ids_list = [module.ssh.security_group_ids, module.http-https.security_group_ids]
   subnet_ids                  = tolist(module.public_subnets.public_subnet_id)
@@ -119,7 +118,6 @@ module "ec2" {
   iam_instance_profile     = module.iam-role.name
 
   disk_size          = 8
-  ebs_optimized      = false
   ebs_volume_enabled = true
   ebs_volume_type    = "gp2"
   ebs_volume_size    = 30
--- _example/secure_example/example.tf
+++ _example/secure_example/example.tf
@@ -144,7 +144,6 @@ module "ec2" {
   instance_count              = 2
   ami                         = "ami-08d658f84a6d84a80"
   instance_type               = "t2.nano"
-  monitoring                  = false
   tenancy                     = "default"
   vpc_security_group_ids_list = [module.ssh.security_group_ids, module.http-https.security_group_ids]
   subnet_ids                  = tolist(module.public_subnets.public_subnet_id)
@@ -156,7 +155,6 @@ module "ec2" {
   iam_instance_profile     = module.iam-role.name
 
   disk_size          = 8
-  ebs_optimized      = false
   ebs_volume_enabled = true
   ebs_volume_type    = "gp2"
   ebs_volume_size    = 30
