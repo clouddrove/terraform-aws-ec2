64be873a01c3ded245b30facd51a8f68e99553c0
yadavprakash <yadavprakash.it@gmail.com>
2021-07-08 18:11:39 +0530

    fix terratest

--- _example/ebs_mount/example.tf
+++ _example/ebs_mount/example.tf
@@ -87,7 +87,7 @@ module "kms_key" {
   description             = "KMS key for ec2"
   deletion_window_in_days = 7
   enable_key_rotation     = true
-  alias                   = "alias/ec2-instance"
+  alias                   = "alias/ec2"
   policy                  = data.aws_iam_policy_document.kms.json
 }
 
