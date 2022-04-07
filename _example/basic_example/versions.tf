b47bfd4ee3a67b079fd73969469238c8ba5da225
Mamraj Yadav <mamraj.yadav@clouddrove.com>
2022-04-07 17:48:21 +0530

    update github-action

--- _example/basic_example/versions.tf
+++ _example/basic_example/versions.tf
@@ -1,6 +1,6 @@
 # Terraform version
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.14.0"
 
   required_providers {
     aws = {
---
+++ _example/ebs_mount/versions.tf
@@ -0,0 +1,11 @@
+# Terraform version
+terraform {
+  required_version = ">= 0.14.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.1.15"
+    }
+  }
+}
