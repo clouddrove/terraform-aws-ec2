85feff224ddb9917d7a07b2df897e43fda71ede3
theprashantyadav <theprashantyadav7@gmail.com>
2023-06-22 12:53:07 +0530

    bug: fix dfsec error in security-group

--- _example/spot_instance/example.tf
+++ _example/spot_instance/example.tf
@@ -30,12 +30,14 @@ module "public_subnets" {
   environment = "test"
   label_order = ["name", "environment"]
 
-  availability_zones = ["eu-west-1b", "eu-west-1c"]
-  vpc_id             = module.vpc.vpc_id
-  cidr_block         = module.vpc.vpc_cidr_block
-  type               = "public"
-  igw_id             = module.vpc.igw_id
-  ipv6_cidr_block    = module.vpc.ipv6_cidr_block
+  availability_zones              = ["eu-west-1b", "eu-west-1c"]
+  vpc_id                          = module.vpc.vpc_id
+  type                            = "public"
+  igw_id                          = module.vpc.igw_id
+  cidr_block                      = module.vpc.vpc_cidr_block
+  ipv6_cidr_block                 = module.vpc.ipv6_cidr_block
+  assign_ipv6_address_on_creation = false
+  enable_vpc_endpoint             = false
 }
 
 ####----------------------------------------------------------------------------------
